23aa710616543783a8fe8e45f823a850c2015e99
OSHIN <izkp44@gmail.com>
2023-12-09 23:12:43 +0700

    first week

---
+++ homework-aj-dave.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+; rectangle
+(define (rectangle w l)
+  (* w l))
+
+(rectangle 2 2)
+
+; circle
+(define pi 3.14)
+
+(define (circle r)
+  (* pi r r))
+
+(circle 7)
+
+; square
+(define (square s)
+  (* s s))
+
+(square 4)
